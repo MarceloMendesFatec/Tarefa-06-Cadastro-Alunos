6cf5370b7436933da278dfdde39f16f3ea95de30
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2022-10-25 21:47:58 -0300

    "Added mock-products list"

---
+++ src/mock-products.ts
@@ -0,0 +1,16 @@
+import { Products } from "./interface-product";
+
+export const mockList: Products[] =[
+  {id: 1,  name:'Rtx 2060 6gb', manufacturer:'Galax', price: 1599.99,qtd: 13 },
+  {id: 2,  name:'Notebook Gamer intel Core i5', manufacturer:'Lenovo', price: 4599.99,qtd: 3 }
+  {id: 3,  name:'Xbox Series S', manufacturer:'Microsoft', price: 2599.99,qtd: 8 },
+  {id: 4,  name:'Processador Ryzen 7 5800x', manufacturer:'AMD', price: 1899.99,qtd: 10 },
+  {id: 5,  name:'Cadeira Gamer T3RUSH', manufacturer:'Corsair', price: 2049.99,qtd: 4 },
+  {id: 6,  name:'Headset Cloud Stinger', manufacturer:'Hyperx', price: 199.99,qtd: 43 },
+  {id: 7,  name:'Notebook Gamer Intel Core i7', manufacturer:'Dell', price: 11599.99,qtd: 2 },
+  {id: 8,  name:'Rtx 3060ti', manufacturer:'Asus', price: 2599.99,qtd: 7 },
+  {id: 9,  name:'Rtx 3070 12gb', manufacturer:'Gigabyte', price: 2399.99,qtd: 8 },
+  {id: 10, name:'RX6500 XT', manufacturer:'ASrock', price: 1199.99,qtd: 22 },
+]
+
+
